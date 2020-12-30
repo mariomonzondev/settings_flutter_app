20d841e6dc7443af8cc640248d09644007cfee04
Mario Monzon <hola@mariomonzon.dev>
2020-12-30 17:50:26 +0100

    First commit

---
+++ android/app/src/main/kotlin/dev/mariomonzon/settings_flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.mariomonzon.settings_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:settings_flutter_app/app.dart';
+
+void main() => runApp(App());
+
